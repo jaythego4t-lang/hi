b3e39fc4edbe3755647cc6a7249b6843da34a583
jaythego4t-lang <jaythego4t@gmail.com>
2026-05-21 00:22:23 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -24,7 +24,7 @@ def ReturnFunctionJson(data, funcname, funcparam={}):
         "TitlePlayerAccountId")
 
     req = requests.post(
-        url=f"https://{settings.TitleId}.playfabapi.com/Server/ExecuteCloudScript",
+        url=f"https://{settings.TitleId}.playfabapi.com/Server/ExecuteCloudScript"
         json={
             "PlayFabId": userId,
             "FunctionName": funcname,
@@ -56,7 +56,7 @@ def main():
             </head>
             <body style="font-family: 'Inter', sans-serif;">
                 <h1 style="color: blue; font-size: 30px;">
-                    sup gango! this is cat tags backend!
+                    sup gango! welcome to cat tags backend!! 
                 </h1>
             </body>
         </html>
